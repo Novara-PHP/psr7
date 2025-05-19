743ab0ddc3d187f77f6d8c963a7203f479b2b53d
RobertWesner <robert@wesner.io>
2025-05-19 15:40:06 +0200

    Documented ConstantStream::isSeekable()

--- src/Stream/ConstantStream.php
+++ src/Stream/ConstantStream.php
@@ -58,6 +58,15 @@ abstract class ConstantStream implements StreamInterface
         return false;
     }
 
+    /**
+     * Impossible to be stateless.
+     *
+     * Due to seek() and rewind() returning void,
+     * it is impossible to even simulate seeking with substrings,
+     * since you cannot return a DRC instance.
+     *
+     * @see https://github.com/Novara-PHP/psr7/issues/1
+     */
     public function isSeekable(): bool
     {
         return false;
